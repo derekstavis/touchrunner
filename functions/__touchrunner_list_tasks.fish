ac1783890b42d0734d3f5c34791bc6e4e1692c4f
Derek Stavis <dekestavis@gmail.com>
2017-12-10 20:48:12 -0200

    list-tasks: add test for package.json presence

--- functions/__touchrunner_list_tasks.fish
+++ functions/__touchrunner_list_tasks.fish
@@ -1,4 +1,6 @@
 function __touchrunner_list_tasks
-  set -l list_scripts 'Object.keys(require("./package.json").scripts).forEach(n => console.log(n))'
-  node -e "$list_scripts" ^/dev/null
+  if test -f ./package.json
+    set -l list_scripts 'Object.keys(require("./package.json").scripts).forEach(n => console.log(n))'
+    node -e "$list_scripts" ^/dev/null
+  end
 end
